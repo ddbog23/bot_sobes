7992bb169b848db6ea70be8580f4673638b44a44
ddbog23 <158063551+ddbog23@users.noreply.github.com>
2025-07-27 16:43:22 +0300

    Update sobes_bot.py

--- sobes_bot.py
+++ sobes_bot.py
@@ -2,6 +2,7 @@ from telegram import ReplyKeyboardMarkup, KeyboardButton, Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
 import os
+import asyncio
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")  # Токен из переменной окружения
 
@@ -13,15 +14,18 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def handle_contact(update: Update, context: ContextTypes.DEFAULT_TYPE):
     contact = update.message.contact.phone_number
     await update.message.reply_text(f"Спасибо! Ты отправил номер: {contact}")
+    # Здесь в будущем можно будет добавить отправку в amoCRM
 
-def main():
+async def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    await app.bot.delete_webhook(drop_pending_updates=True)  # Удалил  webhook перед polling
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.CONTACT, handle_contact))
 
     print("Бот запущен...")
-    app.run_polling()
+    await app.run_polling()
 
-if __name__ == "__main__":
-    main()
+if name == "__main__":
+    asyncio.run(main())
